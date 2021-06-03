b2e16274841cb7f9aae85eabe3fe6dd57debe0e3
Jason Kraft <developer.kraft@gmail.com>
2021-06-03 13:35:38 -0700

    Add codebuild / codedeploy spec

--- CloudFormation/deploy-infra.sh
+++ CloudFormation/deploy-infra.sh
@@ -3,8 +3,21 @@
 STACK_NAME=bondfire
 REGION=us-east-1
 CLI_PROFILE=bondfireCLI
-
 EC2_INSTANCE_TYPE=t2.micro
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile $CLI_PROFILE --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
+
+# Deploys static resources
+echo -e "\n\n=========== Deploying setup.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
 
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
--- README.md
+++
@@ -1,2 +0,0 @@
-# aws-template
-Bootstrap an AWS environment with CI/CD pipeline, staging and production environments, application load balancers, auto-scaling groups, port-forwarding, subnets, and NAT gateway via CloudFormation.
